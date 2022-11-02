b9b4d6e2482fcc8441ea74dbda4025727995c9dd
Ian <iw5420@gmail.com>
2022-11-02 11:18:40 +0800

    first commit

---
+++ src/test/java/com/mastermicroservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mastermicroservices.limitsservice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
